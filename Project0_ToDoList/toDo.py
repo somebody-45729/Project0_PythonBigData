be4534889b233a2c96bd79afb04bdedf6d8c6e7a
somebody-45729 <90016656+somebody-45729@users.noreply.github.com>
2022-07-21 21:37:08 -0500

    update

--- Project0_ToDoList/toDo.py
+++ Project0_ToDoList/toDo.py
@@ -4,6 +4,8 @@ import sys
 import datetime
 import csv
 
+from toDo_ExtraCode import toDo_Task, user
+
 # NEED TO ASK HELP IN SETTING UP CONNECTION WITH .csv file read/write
 
 
@@ -12,10 +14,56 @@ def main():
 
     print("******** TO-DO LIST ********")
     
-    fname = "toDoList.csv"
+    csvName = "toDoList.csv"
     lst_toDo = []
-    lst_toDo = load_Tasks(fname)
+    lst_toDo = load_Task(csvName)
     logging.info("Loading tasks into the list....")
 
+    while(True):
+        toDo = insert_Task()
+        if toDo == None:
+            break
+        logging.info("Inserted new task!")
+        lst_toDo.append(toDo)
+
+    for list in lst_toDo:
+        print(list)
     
+    save_Task(csvName, lst_toDo)
+    logging.info("Saving Task to " + csvName)
+    print("\n\n")
+
+def save_Task(csvName, lst_toDo):
+    '''
+    Save all submitted tasks to the csv file
+
+    No return for this case
+    '''
+    with open(csvName, "w") as f:
+        for toDo in lst_toDo:
+            if type(toDo) == user:
+                f.write("Task, " + toDo._task + "," + str(toDo._date) + "," + toDo._priority + ",\n")
+            else:
+                pass
+
+
+def load_Task(csvName):
+    '''
+    Load into the list, return the list
+    '''
+    lst_toDo = []
+
+    with open(csvName, "r") as f:
+        for row in f:
+            infoLine = row.split(',')
+            if infoLine[0] == "user":
+                toDo = user(infoLine[1], infoLine[2], infoLine[3])
+            else:
+                toDo = None
+
+            if toDo != None:
+                lst_toDo.append(toDo)
+    return lst_toDo
+
+def insert_Task() -> toDo_Task:
     
--- Project0_ToDoList/toDo_ExtraCode.py
+++ Project0_ToDoList/toDo_ExtraCode.py
@@ -1,27 +1,12 @@
 # Extra class/module handler here
 
-class input():
-    def __init__(self, date, event, priority=""):
+class toDo_Task():
+    def __init__(self, task, date, priority):
+        self._task = str(task)
         self._date = str(date)
-        self._event = str(event)
         self._priority = str(priority)
 
-    def __str__(self):
-        return "Date: " + self._date + ", Item" + self._event + ", Level of importance: " + self._priority
-        
-class Event(input):
-    def run(self):
-        print("A New input for the List!")
-
-class LOW(input):
-    def priority(self):
-        print("LOW priority for this case!")
-
-class MEDIUM(input):
-    def priority(self):
-        print(" priority for this case!")
-
-class HIGH(input):
-    def priority(self):
-        print("HIGH priority for this case!")
+class user(toDo_Task):
+    def chosenTask(self):
+        print("***Task is Chosen***")
     
